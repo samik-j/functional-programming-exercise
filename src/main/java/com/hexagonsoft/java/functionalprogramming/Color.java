dcb274ad2a0212e1aa5854fff134106364b2e3e5
joanna <jo.senczuk@gmail.com>
2018-01-23 15:11:20 +0000

    Add Color class

---
+++ src/main/java/com/hexagonsoft/java/functionalprogramming/Color.java
@@ -0,0 +1,15 @@
+package com.hexagonsoft.java.functionalprogramming;
+
+public class Color {
+
+    public String name;
+
+    public Color(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name= " + name;
+    }
+}
